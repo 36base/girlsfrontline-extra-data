a589fc6d5fefbc4d33739182add30b18adcc1a39
okms00909 <okms00909@gmail.com>
2018-08-26 20:41:35 +0900

    update (change directory structure, add locale)

--- src/index.js
+++ src/index.js
@@ -1,4 +1,28 @@
 
-const characterScript = require('./../data/characterScript');
+const gfextradata = ({ locale }) => {
+  switch (locale) {
+    case 'en': {
+      // not yet
+      const characterScript = require('./../data/ko/characterScript');
+      const dollNickname = require('./../data/ko/dollNickname');
 
-module.exports = { characterScript };
+      return { characterScript, dollNickname };
+    }
+    case 'ko': {
+      const characterScript = require('./../data/ko/characterScript');
+      const dollNickname = require('./../data/ko/dollNickname');
+
+      return { characterScript, dollNickname };
+    }
+    case 'ja': {
+      // not yet
+      const characterScript = require('./../data/ko/characterScript');
+      const dollNickname = require('./../data/ko/dollNickname');
+
+      return { characterScript, dollNickname };
+    }
+  }
+  return { };
+} 
+
+module.exports = gfextradata;
